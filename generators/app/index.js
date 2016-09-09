a883fc32947a4cfe062f17888b7641baa8a63eee
Tamas Csizmadia <tamas@me.com>
2016-09-10 00:37:20 +0200

    WIP: Updated prompts, the generator copies `src/agency.txt` and `src/headers/shapes.txt` files.

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,15 +7,35 @@ module.exports = yeoman.Base.extend({
   prompting: function () {
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the superior ' + chalk.red('generator-gtfs-builder') + ' generator!'
+      'Welcome to ' + chalk.green('OPEN Kaposvár GTFS Feed') + ' generator!'
     ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    var prompts = [
+      {
+        type: 'input',
+        name: 'agencyName',
+        message: "Please provide the transit agency's name",
+        default: "Kaposvári Tömegközlekedési Zrt."
+      },
+      {
+        type: 'input',
+        name: 'agencyId',
+        message: "Please provide the transit agency's id",
+        default: "KTZrt"
+      },
+      {
+        type: 'input',
+        name: 'agencyUrl',
+        message: "Please provide the transit agency's website",
+        default: "http://kaposbusz.hu"
+      },
+      {
+        type: 'input',
+        name: 'agencyTZ',
+        message: "Please provide the timezone of operation",
+        default: "Europe/Budapest"
+      }
+    ];
 
     return this.prompt(prompts).then(function (props) {
       // To access props later use this.props.someAnswer;
@@ -25,8 +45,19 @@ module.exports = yeoman.Base.extend({
 
   writing: function () {
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath('_src/_headers/_shapes.txt'),
+      this.destinationPath('src/headers/shapes.txt')
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('_src/_agency.txt'),
+      this.destinationPath('src/agency.txt'),
+      {
+        agencyId: this.props.agencyId,
+        agencyName: this.props.agencyName,
+        agencyUrl: this.props.agencyUrl,
+        timeZone: this.props.agencyTZ
+      }
     );
   },
 
