02e07fd70f85fa74af752d316db5614fc0c5c1a7
Tamas Csizmadia <tamas@me.com>
2016-09-14 23:25:47 +0200

    A very humble unit test for the generator. Will be updated.

--- test/app.js
+++ test/app.js
@@ -4,15 +4,29 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 describe('generator-gtfs-builder:app', function () {
-  before(function () {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
-      .toPromise();
-  });
+  describe('when every input is properly provided', function() {
+    before(function () {
+      return helpers.run(path.join(__dirname, '../generators/app'))
+        .withPrompts({
+          author: "Test Author",
+          agencyName: "Test Agency",
+          agencyId: "AgencyLtd",
+          agencyUrl: "https://agency.com/",
+          agencyTZ: "Europe/Budapest"
+        })
+        .toPromise();
+    });
 
-  it('creates files', function () {
-    assert.file([
-      'dummyfile.txt'
-    ]);
+    it('creates files', function () {
+      assert.file([
+        'gulpfile.js',
+        'package.json',
+        'validator/feedvalidator.py',
+        'src/headers/shapes.txt',
+        //'src/headers/stops.txt',
+        //'src/headers/trips.txt',
+        'src/agency.txt'
+      ]);
+    });
   });
 });
