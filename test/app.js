b246bd190addb9f3d1e6898b35e9b36c27fc2125
Tamas Csizmadia <tamas@me.com>
2016-09-15 21:31:28 +0200

    Generator's `gulpfile.js` modified to exclude js files from templates. Added some unit tests.

--- test/app.js
+++ test/app.js
@@ -3,16 +3,24 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
+var props = {
+  author: "Test Author",
+  agencyName: "Test Agency",
+  agencyId: "AgencyLtd",
+  agencyUrl: "https://agency.com/",
+  agencyTZ: "Europe/Budapest"
+};
+
 describe('generator-gtfs-builder:app', function () {
   describe('when every input is properly provided', function() {
     before(function () {
       return helpers.run(path.join(__dirname, '../generators/app'))
         .withPrompts({
-          author: "Test Author",
-          agencyName: "Test Agency",
-          agencyId: "AgencyLtd",
-          agencyUrl: "https://agency.com/",
-          agencyTZ: "Europe/Budapest"
+          author: props.author,
+          agencyName: props.agencyName,
+          agencyId: props.agencyId,
+          agencyUrl: props.agencyUrl,
+          agencyTZ: props.agencyTZ
         })
         .toPromise();
     });
@@ -27,6 +35,36 @@ describe('generator-gtfs-builder:app', function () {
         //'src/headers/trips.txt',
         'src/agency.txt'
       ]);
+    });
+
+    it('feed is not generated', function () {
+      assert.noFile([
+        props.agencyId + '.zip'
+      ]);
+    });
+
+    it('package name is properly set in gulpfile', function() {
+      assert.fileContent('gulpfile.js', "var packageName = '" + props.agencyId + ".zip';");
+    });
+
+    it("gulpfile contains a task named 'clean'", function() {
+      assert.fileContent('gulpfile.js', "gulp.task('clean'");
+    });
+
+    it("gulpfile contains a task named 'combine'", function() {
+      assert.fileContent('gulpfile.js', "gulp.task('combine'");
+    });
+
+    it("gulpfile contains a task named 'validate'", function() {
+      assert.fileContent('gulpfile.js', "gulp.task('validate'");
+    });
+
+    it("gulpfile contains a task named 'package'", function() {
+      assert.fileContent('gulpfile.js', "gulp.task('package'");
+    });
+
+    it("gulpfile contains a task named 'geojson'", function() {
+      assert.fileContent('gulpfile.js', "gulp.task('geojson'");
     });
   });
 });
