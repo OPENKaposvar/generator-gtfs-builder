e01d43e44b004efc52b89239913299b92ecf35e9
Tamas Csizmadia <tamas@me.com>
2016-09-18 01:18:25 +0200

    The template's `gulpfile.js` is now passed by `eslint`

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -1,44 +1,49 @@
-// --- CONFIGURE HERE ---
-// Name of the generated package
-var packageName = '<%= agencyId %>.zip';
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+/* eslint import/no-unresolved: [2, { ignore: ['gulp-$', 'gtfs2geojson'] }] */
+const packageName = '<%= agencyId %>.zip';
 
-var gulp = require('gulp'),
-  zip = require('gulp-zip'),
-  exec = require('child_process').execFile,
-  fileProcess = require('gulp-file-process'),
-  util = require('gulp-util'),
-  open = require('gulp-open'),
-  concat = require('gulp-concat'),
-  gtfs2geojson = require('gtfs2geojson'),
-  fs = require('fs'),
-  del = require('del');
+const gulp = require('gulp');
+const zip = require('gulp-zip');
+const exec = require('child_process').execFile;
+const fileProcess = require('gulp-file-process');
+const util = require('gulp-util');
+const open = require('gulp-open');
+const concat = require('gulp-concat');
+const gtfs2geojson = require('gtfs2geojson');
+const fs = require('fs');
+const del = require('del');
 
-gulp.task('clean', function() {
-    return del([
-      'dist/**/*',
-      'geojson/**/*'
-    ]);
-});
+gulp.task('clean', () =>
+  del([
+    'dist/**/*',
+    'geojson/**/*',
+  ])
+);
 
-gulp.task('combine', function() {
-  return gulp
-    .src([
-      './src/headers/shapes.txt',
-      './src/shapes/shapes_*.txt'
-    ])
-    .pipe(concat('shapes.txt'))
-    .pipe(gulp.dest('./feed/'));
-});
+gulp.task('combine', () =>
+  gulp.src([
+    './src/headers/shapes.txt',
+    './src/shapes/shapes_*.txt',
+  ])
+  .pipe(concat('shapes.txt'))
+  .pipe(gulp.dest('./feed/'))
+);
 
-gulp.task('validate', ['combine'], function(cb) {
-  var errorMsg = null;
+gulp.task('validate', ['combine'], (cb) => {
+  let errorMsg = null;
 
-  exec("python", ['validator/feedvalidator.py', '-n', '-o', 'dist/validator-report.html', 'feed/'], function(err, stdout, stderr) {
-    if (stdout.indexOf(" error") !== -1) {
-      stdout.split("\n").forEach((line) => util.log(line));
-      errorMsg = "GTFS feed validation failed!";
-    } else if (stdout.indexOf(" warning") !== -1) {
-      util.log("Validation passed but warnings occured. See dist/validator-report.html for details!");
+  exec('python', [
+    'validator/feedvalidator.py',
+    '-n',
+    '-o',
+    'dist/validator-report.html',
+    'feed/',
+  ], (err, stdout) => {
+    if (stdout.indexOf(' error') !== -1) {
+      stdout.split('\n').forEach(line => util.log(line));
+      errorMsg = 'GTFS feed validation failed!';
+    } else if (stdout.indexOf(' warning') !== -1) {
+      util.log('Validation passed but warnings occured. See dist/validator-report.html for details!');
     }
 
     gulp.src('dist/validator-report.html').pipe(open());
@@ -46,32 +51,32 @@ gulp.task('validate', ['combine'], function(cb) {
   });
 });
 
-gulp.task('package', ['validate'], function() {
-    return gulp.src('feed/*.txt')
+gulp.task('package', ['validate'], () =>
+    gulp.src('feed/*.txt')
       .pipe(zip(packageName))
-      .pipe(gulp.dest('dist'));
-});
+      .pipe(gulp.dest('dist'))
+);
 
-gulp.task('geojson', ['validate'], function(cb) {
+gulp.task('geojson', ['validate'], (cb) => {
   gulp.src('feed/stops.txt')
+  .pipe(fileProcess({
+    process(file, content) {
+      const result = gtfs2geojson.stops(content);
+      fs.writeFileSync('geojson/stops.geojson', JSON.stringify(result, null, 2));
+    },
+  }));
+
+  gulp.src('feed/shapes.txt')
     .pipe(fileProcess({
-      process: function(file, content) {
-        var result = gtfs2geojson.stops(content);
-        fs.writeFileSync('geojson/stops.geojson', JSON.stringify(result, null, 2));
-      }
+      process(file, content) {
+        const result = gtfs2geojson.lines(content);
+        fs.writeFileSync('geojson/lines.geojson', JSON.stringify(result, null, 2));
+      },
     }));
 
-    gulp.src('feed/shapes.txt')
-      .pipe(fileProcess({
-        process: function(file, content) {
-          var result = gtfs2geojson.lines(content);
-          fs.writeFileSync('geojson/lines.geojson', JSON.stringify(result, null, 2));
-        }
-      }));
-
-    return cb();
+  return cb();
 });
 
-gulp.task('default', function() {
+gulp.task('default', () => {
   gulp.start('clean', 'combine', 'validate', 'package', 'geojson');
 });
